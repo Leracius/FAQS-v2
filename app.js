a70b043988821178bd22cca8cbb4597beec5961b
axel quintana <lucasaxelqg15@gmail.com>
2021-12-17 15:30:04 -0300

    font weight js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let botones = document.querySelectorAll(".btn-expandible");
 let textoExpandido = document.querySelectorAll(".p-visible");
 let imagen = document.querySelectorAll(".line");
+let letras = document.querySelectorAll(".titulitos")
 
 botones.forEach((elemento, clave)=>{
     elemento.addEventListener("click", () => {
         textoExpandido[clave].classList.toggle("abrir_cerrar")
         imagen[clave].classList.toggle("rotate")
+        letras[clave].classList.toggle("change")
     })
 });
